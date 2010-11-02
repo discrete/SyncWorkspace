76e3426b223bd6c256a7c455261672423236c0a7
Joohong Kim <joohongkim@Joohong-Kims-MacBook-Pro.local>
2010-11-03 00:29:44 +0900

    draft

---
+++ src/InitalizeWorkspace.py
@@ -0,0 +1,19 @@
+'''
+Created on Nov 1, 2010
+
+@author: joohongkim
+'''
+def ReadFromLocalStructure():
+    return
+
+def WriteToLocalStructure():
+    return
+
+def ReadFromRepositoryStructure():
+    return
+
+def WriteToRepositoryStructure():
+    return
+
+def SyncStructure():
+    return
